cd3d674da3ab86b80f2a6828d5ec3727c68e86b5
Ziad Moftah <43881897+ziadmoftah@users.noreply.github.com>
2024-01-18 16:06:06 +0200

    Create BaseTest Class

---
+++ src/test/java/BaseTest.java
@@ -0,0 +1,32 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.asserts.SoftAssert;
+
+public class BaseTest {
+    WebDriver driver ;
+    SoftAssert softAssert ;
+    @BeforeClass
+    public void setupFireFox(){
+        WebDriverManager.firefoxdriver().setup();
+    }
+
+    @BeforeMethod
+    public void softAssertSetup(){
+        softAssert = new SoftAssert() ;
+    }
+    @BeforeMethod
+    public void driverSetup(){
+        driver = new FirefoxDriver() ;
+        driver.get("https://demo.nopcommerce.com/");
+        driver.manage().window().maximize();
+    }
+
+    @AfterMethod
+    public void driverClose(){
+        driver.close();
+    }
+}
--- src/test/java/SampleTest.java
+++ src/test/java/SampleTest.java
@@ -6,17 +6,10 @@ import org.testng.annotations.Test;
 import webpages.HomePage;
 import webpages.LoginPage;
 
-public class SampleTest {
-    WebDriver driver ;
-    @BeforeClass
-    public void setupFireFox(){
-        WebDriverManager.firefoxdriver().setup();
-    }
+public class SampleTest extends BaseTest{
+
     @Test
     public void test1(){
-        driver = new FirefoxDriver( );
-        driver.manage().window().maximize();
-        driver.get("https://demo.nopcommerce.com/");
         HomePage homePage = new HomePage(driver) ;
         homePage.clickOnLoginHeaderButton();
         LoginPage loginPage = new LoginPage(driver) ;
